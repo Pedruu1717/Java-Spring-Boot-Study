fce6dd3aa42e8219fe2ad6e8e7f6b99cf031d2fc
Pedro <pedromelo2000@outlook.pt>
2026-02-24 15:42:09 +0000

    homepage

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,15 @@
 package com.example.demo;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.CommandLineRunner;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @RestController
@@ -11,6 +17,25 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("Estes são os beans fornecidos pelo Spring Boot:");
+
+			String[] beans = ctx.getBeanDefinitionNames();
+			Arrays.sort(beans);
+			for (String bean : beans) {
+				System.out.println(bean);
+			}
+		};
+	}
+
+
+	@GetMapping("/")
+	public String index() {
+		return "Página Inicial do Spring Boot!";
 	}
 
 	@GetMapping("/hello")
